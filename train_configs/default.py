51f5d9a7380150c8e3f757a6082a56451ad022e2
Ross Wightman <rwightman@gmail.com>
2020-11-18 11:15:43 -0800

    Missed training config. TODO move somewhere else...

---
+++ train_configs/default.py
@@ -0,0 +1,52 @@
+# Copyright 2020 The Flax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copyright 2020 The Flax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Default Hyperparameter configuration."""
+
+import ml_collections
+
+
+def get_config():
+  """Get the default hyperparameter configuration."""
+  config = ml_collections.ConfigDict()
+
+  config.learning_rate = 0.1
+  config.momentum = 0.9
+  config.batch_size = 500
+  config.num_epochs = 10
+
+  config.cache = False
+  config.half_precision = True
+
+  config.drop_rate = 0.2
+  config.drop_path_rate = 0.1
+
+  # If num_train_steps==-1 then the number of training steps is calculated from
+  # num_epochs using the entire dataset. Similarly for steps_per_eval.
+  config.num_train_steps = -1
+  config.steps_per_eval = -1
+  return config
