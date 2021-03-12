c469312b691f4d9992445a713645eb1d2a9823a8
Ross Wightman <rwightman@gmail.com>
2021-03-11 17:59:39 -0800

    Switch to Optax based optimizers

--- jeffnet/linen/__init__.py
+++ jeffnet/linen/__init__.py
@@ -1,3 +1,2 @@
 from .efficientnet_linen import EfficientNet, create_model
 from .ema_state import EmaState
-from .optim.optim_factory import create_optim
--- jeffnet/linen/ema_state.py
+++ jeffnet/linen/ema_state.py
@@ -3,29 +3,26 @@ import flax.serialization as serialization
 import flax.struct as struct
 import jax
 
+from typing import Any
+
 
 @struct.dataclass
 class EmaState:
     decay: float = struct.field(pytree_node=False, default=0.)
-    params: flax.core.FrozenDict = None
-    model_state: flax.core.FrozenDict = None
+    variables: flax.core.FrozenDict[str, Any] = None
 
     @staticmethod
-    def create(decay, params, model_state):
+    def create(decay, variables):
         """Initialize ema state"""
         if decay == 0.:
             # default state == disabled
             return EmaState()
-        ema_params = jax.tree_map(lambda x: x, params)
-        ema_model_state = jax.tree_map(lambda x: x, model_state)
-        return EmaState(decay, ema_params, ema_model_state)
+        ema_variables = jax.tree_map(lambda x: x, variables)
+        return EmaState(decay, ema_variables)
 
-    def update(self, new_params, new_model_state):
+    def update(self, new_variables):
         if self.decay == 0.:
-            return self.replace(params=None, model_state=None)
-
-        new_params = jax.tree_multimap(
-            lambda ema, p: ema * self.decay + (1. - self.decay) * p, self.params, new_params)
-        new_model_state = jax.tree_multimap(
-            lambda ema, s: ema * self.decay + (1. - self.decay) * s, self.model_state, new_model_state)
-        return self.replace(params=new_params, model_state=new_model_state)
+            return self.replace(variables=None)
+        new_ema_variables = jax.tree_multimap(
+            lambda ema, p: ema * self.decay + (1. - self.decay) * p, self.variables, new_variables)
+        return self.replace(variables=new_ema_variables)
